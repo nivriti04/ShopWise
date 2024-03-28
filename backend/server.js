a13ea0c666649d33f8e8b54ce8973c83bd7b1747
Nivriti Bajwa <nivriti@Nivritis-MacBook-Pro.local>
2024-03-28 18:33:49 -0400

    redux setup

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
+
 const app = express();
 
-app.get("/", (req, response) => {
-  response.send("API is running...");
+app.use('/api/products', productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.use("/api/products", productRoutes);
-
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
