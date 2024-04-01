a46e98e23fa932d978f072e020c38341e376df39
Nivriti Bajwa <nivriti@Nivritis-MacBook-Pro.local>
2024-04-01 18:59:30 -0400

    backend authentication/ jwt cookie/ login/register/update user profile

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+  logoutUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+//all of these are connected by /api/users
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 5000;
 
@@ -11,10 +13,18 @@ connectDB();
 
 const app = express();
 
-app.use('/api/products', productRoutes);
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+//cookie parser middleware
+app.use(cookieParser());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.use(notFound);
